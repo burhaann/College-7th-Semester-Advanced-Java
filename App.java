f6d159430b7947121349da6ec42209fe829c74ee
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-12-15 16:46:11 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,13 +1,12 @@
-
 public class App {
-public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
-    try{
-        System.out.println(10/0);
-    } catch (Exception e){
-        System.out.println(e);
-    } finally {
-        System.out.println("Error has Aklread");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        try {
+            System.out.println(10 / 0);
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("Error has Aklread");
+        }
     }
-}
 }
